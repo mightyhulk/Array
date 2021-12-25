5183a263ca3541b6e37257bd46f923fd862d65cf
mighty_hulk <coder10978@gmail.com>
2021-12-25 08:47:15 +0530

    First Commit

---
+++ Arrays/1.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int i,j,k;
+        
+   int arr[5]; 
+
+    for(i=0;i<5;i++){
+        cin>>arr[i];
+    }
+    
+    for(i=1;i<=5;i++){
+        cout<<arr[5-i]<<",";
+    }
+
+  return 0;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
